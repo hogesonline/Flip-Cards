d64506ee3baf38f47c5d57cc7b0769de92e9e01c
syn <afishydeath@gmail.com>
2024-10-22 23:47:13 +1100

    convert to scaffolds
    pruned actual code from app.py to leave only comments, and added
    comments where necessary. Removed most content from most templates, left
    forms, and left the structure template.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from sqlalchemy import create_engine, text
-from flask import Flask, redirect, render_template, request, session
-from flask_session import Session
-from werkzeug.security import check_password_hash, generate_password_hash
-from functools import wraps
 from datetime import datetime
+from functools import wraps
+
+from flask import Flask, redirect, render_template, request, session
+from sqlalchemy import create_engine, text
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flask_session import Session
 
 # Configure application
 app = Flask(__name__)
@@ -25,17 +27,7 @@ CATEGORIES = ["Math", "Science", "History", "Geography", "English", "Art", "Musi
 #helper functions
 def apology(message, code=400):
     """Render message as an apology to user."""
-    def escape(s):
-        """
-        Escape special characters.
-
-        https://github.com/jacebrowning/memegen#special-characters
-        """
-        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
-                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
-            s = s.replace(old, new)
-        return s
-    return render_template("apology.html", top=code, bottom=escape(message)), code
+    pass
 
 
 def login_required(f):
@@ -55,11 +47,7 @@ def login_required(f):
 @login_required
 def index():
     """Show list of questions"""
-    select_text = text("SELECT * FROM flipcards WHERE user_id = :uid")
-    questions = connection.execute(select_text, {"uid":session["user_id"]})
-    questions = questions.fetchall()
-
-    return render_template("index.html" , questions=questions)
+    pass
 
 
 @app.route("/add", methods=["GET", "POST"])
@@ -67,102 +55,54 @@ def index():
 def add_question():
     if request.method == "POST":
         """Add questions to the list"""
-        question = request.form.get("question")
-        answer = request.form.get("answer")
-        category = request.form.get("category")
-        difficulty = request.form.get("difficulty")
-        if not question:
-            return apology("Missing a question")
-        if not answer:
-            return apology("Missing an answer")
-        if not difficulty:
-            return apology("Missing a difficulty")
-        if category not in CATEGORIES:
-            return apology("Invalid Category")
-        #store the registration
-        add_question_db = text("INSERT INTO flipcards (user_id, question, answer, category, difficulty, created_date) values(:uid, :question, :answer, :category, :difficulty, :created)")   
-        connection.execute(add_question_db, [{"uid": session["user_id"], "question":question, "answer":answer, "category":category, "difficulty":difficulty, "created":datetime.now()}])
-        connection.commit()
-        return redirect("/add")
+        pass
     else:
-        return render_template("add_question.html" , categories=CATEGORIES)
-
+        """Show add question form"""
+        pass
 
 @app.route("/quiz", methods=["GET", "POST"])
 @login_required
 def quiz():
     if request.method == "GET":
         """Show quiz setup"""
-        return render_template("quiz_setup.html", categories = CATEGORIES)
+        pass
     else:
         """Show quiz questions"""
-        num_questions = request.form.get("num_quest")
-        category = request.form.get("category")
-        difficulty_start = request.form.get("difficulty_start")
-        difficulty_finish = request.form.get("difficulty_stop")
-        if not num_questions:
-            return apology("Missing a number of questions")
-        if not difficulty_start:
-            return apology("Missing a difficulty minimum")
-        if not difficulty_finish:
-            return apology("Missing a difficulty maximum")
-        if category not in CATEGORIES:
-            return apology("Invalid Category")
-        select_qry = text("SELECT * FROM flipcards WHERE category = :cat AND difficulty BETWEEN :diff_start AND :diff_finish ORDER BY RANDOM() LIMIT :num")
-        questions = connection.execute(select_qry, {"cat":category, "diff_start":difficulty_start, "diff_finish":difficulty_finish, "num":num_questions})
-        questions = questions.fetchall()
-        session["questions"] = questions
-        session["quest_num"] = 0
-        return render_template("quiz.html", question=questions[0])
+        pass
 
 @app.route('/next_question', methods=['POST'])
 def next_question():
-    session['quest_num'] += 1
-    if session['quest_num'] < len(session["questions"]):
-        question = session["questions"][session['quest_num']]
-        return render_template("quiz.html", question=question)
-    else:
-        question = {"question": "No more questions", "answer": ""}
-        return render_template("quiz.html", question=question)
+    """
+    If there is a next question, redirect the user to complete it.
+    If there is not, inform the user
+    """
+    pass
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
 
     # Forget any user_id
-    session.clear()
-    
+
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        uname = request.form.get("username")
-        pwd = request.form.get("password")
+
         # Ensure username was submitted
-        if not uname:
-            return apology("must provide username", 403)
 
         # Ensure password was submitted
-        elif not pwd:
-            return apology("must provide password", 403)
 
         # Query database for username
-        select_qry = text("SELECT * FROM users WHERE username = :uname")
-        rows = connection.execute(select_qry, [{"uname":uname}])
-        rows = rows.fetchall()
-
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(rows[0][2], pwd):
-            return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0][0]
 
         # Redirect user to home page
-        return redirect("/")
+        pass
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("login.html")
+        pass
 
 
 @app.route("/logout")
@@ -170,57 +110,39 @@ def logout():
     """Log user out"""
 
     # Forget any user_id
-    session.clear()
 
     # Redirect user to login form
-    return redirect("/")
+    pass
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    # Forget any user_id
-    session.clear()
     """Register user"""
+
+    # Forget any user_id
+
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        uname = request.form.get("username")
-        pwd = request.form.get("password")
-        pwd2 = request.form.get("password2")
+
         # Ensure username was submitted
-        if not uname:
-            return apology("must provide username", 403)
 
         # Ensure password was submitted
-        elif pwd:
-            return apology("must provide password", 403)
-        elif pwd != pwd2:
-            return apology("passwords must match", 403)
 
         # Query database for username
-        insert_qry = text("INSERT INTO users (username, hash) values(:uname,:pwd)")
-        result = connection.execute(insert_qry, [{"uname":uname, "pwd":generate_password_hash(pwd)}])
-        connection.commit()
+
         # Redirect user to home page
-        return redirect("/")
+        pass
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
-        return render_template("register.html")
-
-    #return apology("TODO")
+        pass
 
 
 @app.route("/remove", methods=["POST"])
 @login_required
 def remove():
     """Remove questions"""
-    question = request.form.get("question")
-    if not question:
-        return apology("Something went wrong")
-    remove_qry = text("DELETE FROM flipcards WHERE user_id = :uid AND card_id = :qid")
-    connection.execute(remove_qry, {"uid": session["user_id"], "qid":question})
-    connection.commit()
-    return redirect("/")
+    pass
 
 
 if __name__ == "__main__":
